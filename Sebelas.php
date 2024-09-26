a024407179bddc3fc9b9f5eaa3d3fcfefea1a71c
NawrahChyntia <h1101231007@student.untan.ac.id>
2024-09-26 22:40:33 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- Sebelas.php
+++ Sebelas.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use App\Model\Akademik\Pegawai;
+use App\Admin\Dosen;
+use App\Model\Akademik\Dosen;
 
-// Buat objek $dian dari kelas Dosen
-$dian = new Dosen(198411132015041001, "Dian Prawira", "62111111", "Jln Purnama", "0013118405");
-$dian->nidn=1000121184001
+$dian = new Dosen(198411132015041001, "Dian Prawira", 62111111, "Jln Purnama", "0013118405");
 
 $dian->mengajar();
+$dian->nidn = 1000121184001;
+$dian->mengajar();
+echo $dian->getNoHp();
