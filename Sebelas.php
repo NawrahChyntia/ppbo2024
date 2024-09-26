339ee1999c0673a8d3f8529c5fbeea9f4e62cd3b
NawrahChyntia <h1101231007@student.untan.ac.id>
2024-09-26 22:22:30 +0700

    Praktikum 6Cx - Implementasi Enkapsulasi

--- Sebelas.php
+++ Sebelas.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'vendor/autoload.php';
 
-use App\Model\Akademik\Dosen;
+use App\Model\Akademik\Pegawai;
 
 // Buat objek $dian dari kelas Dosen
 $dian = new Dosen(198411132015041001, "Dian Prawira", "62111111", "Jln Purnama", "0013118405");
+$dian->nidn=1000121184001
 
-// Panggil fungsi mengajar()
 $dian->mengajar();
